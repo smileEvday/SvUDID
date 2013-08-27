32d49194fa5c38f3d0d6be8905e31b5618ee43a9
MapleFang <smileEvday@gmail.com>
2013-08-27 23:16:34 +0800

    add access group to keychain item, the accessgroup.plist must be in the same dir with project file

--- SvUDID/SvUDID/UDID_Tools/SvUDIDTools.m
+++ SvUDID/SvUDID/UDID_Tools/SvUDIDTools.m
@@ -14,7 +14,8 @@
 #include <net/if_dl.h>
 
 // replace the identity with your company's domain
-static const char kKeychainUDIDItemIdentifier[]  = "smileEvdayUDID111.com.111.cnblogs.www\0";
+static const char kKeychainUDIDItemIdentifier[]  = "UUID";
+static const char kKeyChainUDIDAccessGroup[] = "FW42Y6F2WC.com.cnblogs.smileEvday";
 
 @implementation SvUDIDTools
 
@@ -63,11 +64,12 @@ static const char kKeychainUDIDItemIdentifier[]  = "smileEvdayUDID111.com.111.cn
  */
 + (NSString*)_UDID_iOS7
 {
+    [SvUDIDTools removeUDIDFromKeyChain];
     NSString *udid = [SvUDIDTools getUDIDFromKeyChain];
-//    if (!udid) {
+    if (!udid) {
         udid = [[UIDevice currentDevice].identifierForVendor UUIDString];
         [SvUDIDTools settUDIDToKeyChain:udid];
-//    }
+    }
 
     return udid;
 }
@@ -85,7 +87,7 @@ static const char kKeychainUDIDItemIdentifier[]  = "smileEvdayUDID111.com.111.cn
     unsigned char       macAddress[6];
     struct if_msghdr    *interfaceMsgStruct;
     struct sockaddr_dl  *socketStruct;
-    NSString            *errorFlag = NULL;
+    NSString            *errorFlag = nil;
     
     // Setup the management Information Base (mib)
     mgmtInfoBase[0] = CTL_NET;        // Request network subsystem
@@ -120,6 +122,10 @@ static const char kKeychainUDIDItemIdentifier[]  = "smileEvdayUDID111.com.111.cn
     if (errorFlag != NULL)
     {
         NSLog(@"Error: %@", errorFlag);
+        if (msgBuffer) {
+            free(msgBuffer);
+        }
+        
         return errorFlag;
     }
     
@@ -161,6 +167,25 @@ static const char kKeychainUDIDItemIdentifier[]  = "smileEvdayUDID111.com.111.cn
                                             length:strlen(kKeychainUDIDItemIdentifier)];
     [dictForQuery setObject:keychainItemID forKey:(id)kSecAttrGeneric];
     
+    // The keychain access group attribute determines if this item can be shared
+    // amongst multiple apps whose code signing entitlements contain the same keychain access group.
+    NSString *accessGroup = [NSString stringWithUTF8String:kKeyChainUDIDAccessGroup];
+    if (accessGroup != nil)
+    {
+#if TARGET_IPHONE_SIMULATOR
+        // Ignore the access group if running on the iPhone simulator.
+        //
+        // Apps that are built for the simulator aren't signed, so there's no keychain access group
+        // for the simulator to check. This means that all apps can see all keychain items when run
+        // on the simulator.
+        //
+        // If a SecItem contains an access group attribute, SecItemAdd and SecItemUpdate on the
+        // simulator will return -25243 (errSecNoAccessForItem).
+#else
+        [dictForQuery setObject:accessGroup forKey:(id)kSecAttrAccessGroup];
+#endif
+    }
+    
     [dictForQuery setValue:(id)kCFBooleanTrue forKey:(id)kSecMatchCaseInsensitive];
     [dictForQuery setValue:(id)kSecMatchLimitOne forKey:(id)kSecMatchLimit];
     [dictForQuery setValue:(id)kCFBooleanTrue forKey:(id)kSecReturnData];
@@ -169,6 +194,10 @@ static const char kKeychainUDIDItemIdentifier[]  = "smileEvdayUDID111.com.111.cn
     NSData   *udidValue = nil;
     NSString *udid      = nil;
     queryErr = SecItemCopyMatching((CFDictionaryRef)dictForQuery, (CFTypeRef*)&udidValue);
+    
+    NSMutableDictionary *dict = nil;
+    [dictForQuery setValue:(id)kCFBooleanTrue forKey:(id)kSecReturnAttributes];
+    queryErr = SecItemCopyMatching((CFDictionaryRef)dictForQuery, (CFTypeRef*)&dict);
     
     if (queryErr == errSecItemNotFound) {
         NSLog(@"KeyChain Item: %@ not found!!!", [NSString stringWithUTF8String:kKeychainUDIDItemIdentifier]);
@@ -195,9 +224,32 @@ static const char kKeychainUDIDItemIdentifier[]  = "smileEvdayUDID111.com.111.cn
     [dictForAdd setValue:(id)kSecClassGenericPassword forKey:(id)kSecClass];
     [dictForAdd setValue:[NSString stringWithUTF8String:kKeychainUDIDItemIdentifier] forKey:kSecAttrDescription];
     
-    NSData *keyChainItemID = [NSData dataWithBytes:kKeychainUDIDItemIdentifier length:strlen(kKeychainUDIDItemIdentifier)];
-    [dictForAdd setValue:keyChainItemID forKey:(id)kSecAttrGeneric];
-   
+    [dictForAdd setValue:@"UUID" forKey:(id)kSecAttrGeneric];
+    
+    // Default attributes for keychain item.
+    [dictForAdd setObject:@"" forKey:(id)kSecAttrAccount];
+    [dictForAdd setObject:@"" forKey:(id)kSecAttrLabel];
+    
+    
+    // The keychain access group attribute determines if this item can be shared
+    // amongst multiple apps whose code signing entitlements contain the same keychain access group.
+    NSString *accessGroup = [NSString stringWithUTF8String:kKeyChainUDIDAccessGroup];
+    if (accessGroup != nil)
+    {
+#if TARGET_IPHONE_SIMULATOR
+        // Ignore the access group if running on the iPhone simulator.
+        //
+        // Apps that are built for the simulator aren't signed, so there's no keychain access group
+        // for the simulator to check. This means that all apps can see all keychain items when run
+        // on the simulator.
+        //
+        // If a SecItem contains an access group attribute, SecItemAdd and SecItemUpdate on the
+        // simulator will return -25243 (errSecNoAccessForItem).
+#else
+        [dictForAdd setObject:accessGroup forKey:(id)kSecAttrAccessGroup];
+#endif
+    }
+
     const char *udidStr = [udid UTF8String];
     NSData *keyChainItemValue = [NSData dataWithBytes:udidStr length:strlen(udidStr)];
     [dictForAdd setValue:keyChainItemValue forKey:(id)kSecValueData];
@@ -268,6 +320,7 @@ static const char kKeychainUDIDItemIdentifier[]  = "smileEvdayUDID111.com.111.cn
     NSDictionary *queryResult = nil;
     SecItemCopyMatching((CFDictionaryRef)dictForQuery, (CFTypeRef*)&queryResult);
     if (queryResult) {
+        
         NSMutableDictionary *dictForUpdate = [[NSMutableDictionary alloc] init];
         [dictForUpdate setValue:[NSString stringWithUTF8String:kKeychainUDIDItemIdentifier] forKey:kSecAttrDescription];
         [dictForUpdate setValue:keychainItemID forKey:(id)kSecAttrGeneric];
